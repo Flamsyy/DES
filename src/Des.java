38159a253ab2e6484a669916ef8693f904efe694
Valmirthecoder <valmir.haxholli@student.cacttus.education>
2019-03-20 08:09:45 +0100

    Created the first version of DES UI

---
+++ src/Des.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Des {
+    private JPanel panel;
+    private JTextField inputStringTextField;
+    private JButton ENCRYPTButton;
+    private JTextField textField1;
+    private JRadioButton ECBRadioButton;
+    private JRadioButton CBCRadioButton;
+    private JRadioButton OFBRadioButton;
+    private JRadioButton CFBRadioButton;
+}
--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,7 @@ public class Main {
                 IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
+
     }
 }
 
