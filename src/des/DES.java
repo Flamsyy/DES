071c3f9f294f6b6d14e6497a5744430d20b08360
Valmirthecoder <valmir.haxholli@student.cacttus.education>
2019-03-23 19:46:28 +0100

    Updated encription

--- src/des/DES.java
+++ src/des/DES.java
@@ -1,21 +1,39 @@
 package des;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Scanner;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class DES
 {
-	public static String encrypt(String plainText, String mode, String padding, String key)
+	public static String encrypt(String plainText, String mode, String padding, String key) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException
 	{
-		return "I work";
+		SecretKey secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "DES");
+		Cipher cipher = Cipher.getInstance("DES/"+mode+"/"+padding);
+		if(mode.equals("ECB")) 
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		else
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(new byte[16]));
+
+		if(padding.equals("NoPadding") && plainText.getBytes().length % 16 != 0)
+		{
+		System.out.println("wrong block size");
+		return "";
+		}
+		byte[] cipherbytes = cipher.doFinal(plainText.getBytes());
+		return Base64.getEncoder().encodeToString(cipherbytes);
+
 	}
 	
 	public static String decrypt(String cipherText, String mode, String padding, String key)
@@ -24,22 +42,3 @@ public class DES
 	}
 }
 
-/*
-Scanner sc = new Scanner(System.in);
-
-System.out.println("Plaintext: ");
-String plaintext = sc.next();
-
-try {
-    KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
-    SecretKey key = keyGenerator.generateKey();
-
-    Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-    cipher.init(Cipher.ENCRYPT_MODE, key);
-
-    System.out.println("Ciphertext: " + new String(cipher.doFinal(plaintext.getBytes())));
-} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException |
-        IllegalBlockSizeException | BadPaddingException e) {
-    e.printStackTrace();
-}
-*/
