fa08de1fa00581b05517409118095b7a5dfc6a19
Flamsyy <flamurbajra99@gmail.com>
2019-03-24 20:53:59 +0100

    reviewed and added a little line indent wherever needed

--- src/des/DES.java
+++ src/des/DES.java
@@ -5,11 +5,9 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-import java.util.Scanner;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
@@ -21,29 +19,31 @@ public class DES
 	{
 		SecretKey secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "DES");
 		Cipher cipher = Cipher.getInstance("DES/"+mode+"/"+padding);
+		
 		if(mode.equals("ECB")) 
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 		else
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(new byte[8]));
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(new byte[8]));
 
-		if(padding.equals("NoPadding") && plainText.getBytes().length%8!=0)
-		{
-		System.out.println("wrong block size");
-		return "";
+		if(padding.equals("NoPadding") && plainText.getBytes().length%8!=0) {
+			System.out.println("wrong block size");
+			return "";
 		}
+		
 		byte[] cipherbytes = cipher.doFinal(plainText.getBytes());
 		return Base64.getEncoder().encodeToString(cipherbytes);
-
 	}
 	
 	public static String decrypt(String cipherText, String mode, String padding, String key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
 	{
 		SecretKey secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "DES");
 		Cipher cipher = Cipher.getInstance("DES/"+mode+"/"+padding);
+		
 		if(mode.equals("ECB"))
-		cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
 		else
-		cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(new byte[8]));
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(new byte[8]));
+		
 		byte[] cipherbytes = cipher.doFinal(Base64.getDecoder().decode(cipherText));
 		return new String(cipherbytes);
 	}
